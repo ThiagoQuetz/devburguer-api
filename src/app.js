216d67295b8aa890c1bd9792b522a27d23113543
ThiagoQuetz <lizplanejadosjf@gmail.com>
2024-08-15 20:14:22 -0300

    fix: api-finish

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import routes from './routes'
 
 import {resolve} from 'node:path'
+import cors from 'cors'
+
 
 import './database';
 
@@ -12,14 +14,21 @@ class App {
     constructor(){
         this.app = express();
 
+        this.app.use(cors());
         this.middlewares();
         this.routes();
     }
 
     middlewares(){
         this.app.use(express.json());
-        this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')));
-    }
+        this.app.use(
+            '/product-file', 
+            express.static(resolve(__dirname, '..', 'uploads')));
+        
+        this.app.use(
+        '/category-file', 
+        express.static(resolve(__dirname, '..', 'uploads')));
+};
 
     routes() {
         this.app.use(routes);
--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -6,45 +6,118 @@ import User from '../models/User';
 
 
 class CategoryController {
-    async store (request, response) {
+    async store(request, response) {
         const schema = Yup.object({
-            name: Yup.string().required(),  
+            name: Yup.string().required(),
         });
 
-                 
 
-        try{
-            schema.validateSync(request.body, {abortEarly: false});
-            }  catch(err){
-                return response.status(400).json({ error: err.errors});
+
+        try {
+            schema.validateSync(request.body, { abortEarly: false });
+        } catch (err) {
+            return response.status(400).json({ error: err.errors });
+        }
+
+        const { admin: isAdmin } = await User.findByPk(request.userId)
+
+        if (!isAdmin) {
+            return response.status(401).json()
+        }
+
+
+        const { filename: path } = request.file;
+        const { name } = request.body;
+
+        const categoryExists = await Category.findOne({
+            where: {
+                name,
             }
+        });
 
-            const isAdmin = await User.findByPk
+        if (categoryExists) {
+            return response
+            .status(400)
+            .json({ error: 'Category already exists' });
+        }
 
-            const { name } = request.body;
+        const { id } = await Category.create({
+            name,
+            path,
+        });
 
-            const categoryExists = await Category.findOne({
+        return response.status(201).json({ id, name });
+    }
+
+    async update(request, response) {
+        const schema = Yup.object({
+            name: Yup.string(),
+        });
+
+
+
+        try {
+            schema.validateSync(request.body, { abortEarly: false });
+        } catch (err) {
+            return response.status(400).json({ error: err.errors });
+        }
+
+        const { admin: isAdmin } = await User.findByPk(request.userId)
+
+        if (!isAdmin) {
+            return response.status(401).json()
+        }
+
+        const { id } = request.params;
+
+        const categoryExists = await Category.findByPk(id);
+
+        if (!categoryExists) {
+            return response
+                .status(400)
+                .json({ message: 'Make sure your category ID is correct' });
+        }
+
+        let path;
+
+        if (request.file) {
+            path = request.file.filename;
+        }
+
+        const { name } = request.body;
+
+        if (name) {
+            const categoryNameExists = await Category.findOne({
                 where: {
                     name,
                 }
             });
-
-            if (categoryExists) {
-                return response.status(400).json({ error: 'Category already exists'});
+    
+            if (categoryNameExists && categoryNameExists.id != id) {
+                return response.status(400).json({ error: 'Category already exists' });
             }
+        }
 
-            const {id} = await Category.create({
-                name,
-            });
+        await Category.update(
+           {
+            name, 
+            path,
+           },
+           {
+            where: {
+                id,
+            },
+           },
+        );
 
-        return response.status(201).json({ id, name });
+        return response.status(200).json();
     }
 
     async index(request, response) {
         const categories = await Category.findAll();
 
-        console.log({userId: request.userId});
-        
+        console.log({ userId: request.userId });
+
         return response.json(categories)
     }
 
